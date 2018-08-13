cab2bef3d19be65d6c89e8cadbc9f32da9757fa5
md mia <sharifmia091@gmail.com>
2018-08-13 02:57:00 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board
+  puts " 0 | X |   "
+  puts "-----------"
+  puts "   | X |   "
+  puts "-----------"
+  puts " X | 0 |   "
+
+end
