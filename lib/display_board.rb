9845e0a315a11a71c89db1ae7b2323889651144b
md mia <sharifmia091@gmail.com>
2018-08-13 03:01:59 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  puts " 0 | X |   "
+  puts "   |   |   "
   puts "-----------"
-  puts "   | X |   "
+  puts "   |   |   "
   puts "-----------"
-  puts " X | 0 |   "
+  puts "   |   |   "
 
 end
 board=["","","","","","","","",""]
